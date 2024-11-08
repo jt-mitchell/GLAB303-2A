f00e995001fe3d1fd7bf4376661f846bd78183bc
Jeff Mitchell <jeffreytm@gmail.com>
2024-11-08 11:02:03 -0500

    first update note

---
+++ src/LogicalOperatorsDemo.java
@@ -0,0 +1,19 @@
+
+public class LogicalOperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  boolean x = true;
+	       boolean y = false;
+	       System.out.println("x & y : " + (x & y));
+	       System.out.println("x && y : " + (x && y));
+	       System.out.println("x | y : " + (x | y));
+	       System.out.println("x || y: " + (x || y));
+	       System.out.println("x ^ y : " + (x ^ y));
+	       System.out.println("!x : " + (!x));
+	   }
+
+	}
+
+
